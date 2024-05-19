af4e30d53a2230d16dc0f2f5a9944e4860e536bb
Nikita <skydes2301@mail.ru>
2024-05-19 19:49:32 +0500

    start

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+    return (
+        <div>
+            <Todolist />
+        </div>
+    )
+}
+
+
+function Todolist() {
+
+    return (
+        <div>TODOLIST</div>
+    )
+}
+
+
+export default App
